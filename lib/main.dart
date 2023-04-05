8ac56a57f54a9be00c48414519761e60361c4890
Strong Muhoti <strongmuhoti@gmail.com>
2023-04-05 20:57:48 +0300

    grid layout

--- lib/main.dart
+++ lib/main.dart
@@ -25,13 +25,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(title)),
-      body: const Center(
-        child: Text('My Page!'),
-      ),
       drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
         child: ListView(
           // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
@@ -62,6 +56,17 @@ class MyHomePage extends StatelessWidget {
             ),
           ],
         ),
+      ),
+      body: GridView.count(
+        crossAxisCount: 4,
+        children: List.generate(100, (index) {
+          return Center(
+            child: Text(
+              'Item $index',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+          );
+        }),
       ),
     );
   }
